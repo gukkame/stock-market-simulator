52a7b769eff415d0a3a08de267805109dbe0757a
gukkame <guka245@gmail.com>
2023-08-11 19:04:35 +0300

    added alerDialog

--- stock_market/lib/screens/transactions.dart
+++ stock_market/lib/screens/transactions.dart
@@ -1,5 +1,9 @@
+import 'dart:ffi';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:stock_market/utils/colors.dart';
+import 'package:stock_market/utils/navigation.dart';
 
 import '../components/container.dart';
 import '../components/stock_info_field.dart';
@@ -29,18 +33,119 @@ class _TransactionsState extends State<Transactions> {
             title: widget.companyTitle,
           ),
           const SizedBox(
-            height: 25,
+            height: 60,
+          ),
+          Text(
+            "${widget.page.toUpperCase()}  AMOUNT  \$",
+            style: const TextStyle(
+                fontSize: 18, color: Colors.white, fontWeight: FontWeight.w300),
+          ),
+          const SizedBox(
+            height: 17,
+          ),
+          _amountScale,
+          const SizedBox(
+            height: 10,
           ),
           const Text(
-            "AMOUNT \$",
+            "Units (amount/price) XXX \$",
+            style: TextStyle(
+                fontSize: 14,
+                color: Colors.white30,
+                fontWeight: FontWeight.w300),
+          ),
+          const SizedBox(
+            height: 260,
+          ),
+          const Text(
+            "Cash Available  XXX \$",
             style: TextStyle(
                 fontSize: 18, color: Colors.white, fontWeight: FontWeight.w300),
           ),
-             const SizedBox(
+          const SizedBox(
             height: 20,
           ),
-          _amountScale,
+          _setOrderBtn,
         ],
+      ),
+    );
+  }
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+        barrierColor: Colors.black45,
+        context: context,
+        builder: (BuildContext context) {
+          return Dialog(
+            backgroundColor: primeColorDark,
+            shadowColor: Colors.transparent,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(9)),
+            alignment: Alignment.center,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(20, 20, 20, 0),
+              height: 140,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Order filled",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                        fontWeight: FontWeight.w300),
+                  ),
+                  Text(
+                    "${widget.page.toUpperCase()} $_currentCount \$",
+                    style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500),
+                  ),
+                  TextButton(
+                    child: const Text(
+                      "OK",
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  Widget get _setOrderBtn {
+    return TextButton(
+      onPressed: () {
+        _showDialog(context);
+      },
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.fromLTRB(0, 4, 0, 4),
+        tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        alignment: Alignment.centerRight,
+        splashFactory: NoSplash.splashFactory,
+      ),
+      child: Container(
+        height: 40,
+        width: 150,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+            gradient: primeGradient, borderRadius: BorderRadius.circular(6.0)),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 0.0, vertical: 6.0),
+          child: Text(
+            "Set Order",
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w200, fontSize: 16),
+          ),
+        ),
       ),
     );
   }
@@ -73,8 +178,12 @@ class _TransactionsState extends State<Transactions> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           _createIncrementDicrementButton(Icons.remove, dicrement),
-          Text(_currentCount.toString(),style: TextStyle(color: Colors.white, fontWeight: FontWeight.w300, fontSize: 20),),
-          _createIncrementDicrementButton(Icons.add,  increment),
+          Text(
+            _currentCount.toString(),
+            style: TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w300, fontSize: 20),
+          ),
+          _createIncrementDicrementButton(Icons.add, increment),
         ],
       ),
     );
@@ -84,7 +193,7 @@ class _TransactionsState extends State<Transactions> {
     return RawMaterialButton(
       materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
       constraints: BoxConstraints(minWidth: 42.0, minHeight: 42.0),
-     onPressed: () => onPressed(),
+      onPressed: () => onPressed(),
       elevation: 2.0,
       child: Icon(
         icon,
