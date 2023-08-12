3d399edada1d2487be750802886dc44d4230c115
gukkame <guka245@gmail.com>
2023-08-12 19:14:42 +0300

    added log out button

--- stock_market/lib/pages/portfolio.dart
+++ stock_market/lib/pages/portfolio.dart
@@ -13,10 +13,28 @@ class PortfolioPage extends StatefulWidget {
 class _PortfolioPageState extends State<PortfolioPage> {
   @override
   Widget build(BuildContext context) {
-     return  Scaffold(
-      appBar: AppBar(title: const Text("Portfolio"),),
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("   Portfolio"),
+            IconButton(
+              onPressed: logOut,
+              icon: const Icon(
+                Icons.logout,
+              ),
+            ),
+          ],
+        ),
+        automaticallyImplyLeading: false,
+      ),
       body: const Portfolio(),
       bottomNavigationBar: const BottomNavBar(index: 2),
     );
+  }
+
+  void logOut() {
+    debugPrint("logOut");
   }
 }
--- stock_market/lib/pages/view_stocks.dart
+++ stock_market/lib/pages/view_stocks.dart
@@ -15,11 +15,26 @@ class StockListPageState extends State<StockListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Stock List"),
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Text("    Stock List"),
+            IconButton(
+              onPressed: logOut,
+              icon: const Icon(
+                Icons.logout,
+              ),
+            ),
+          ],
+        ),
         automaticallyImplyLeading: false,
       ),
       body: const StockList(),
       bottomNavigationBar: const BottomNavBar(index: 1),
     );
+  }
+
+  void logOut() {
+    debugPrint("logOut");
   }
 }
